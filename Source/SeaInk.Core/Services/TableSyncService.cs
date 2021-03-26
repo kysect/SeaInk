1e4b73d0aca54b85538c00a1a75b140e160a9c11
Valery Shevchenko <ar4er.valera@gmail.com>
2021-03-27 00:42:14 +0300

    Commit 2

--- Source/SeaInk.Core/Services/TableSyncService.cs
+++ Source/SeaInk.Core/Services/TableSyncService.cs
@@ -5,72 +5,180 @@ using ClosedXML.Excel;
 
 namespace SeaInk.Core.Services
 {
-    public class TableSyncService
+    public class ExcelGenerator
+
     {
-        //Для тестиварония
-        public static void GenerateTableExample()
-        {
-            List<string> a = new List<string>();
-            a.Add("Иванов Иван");
-            a.Add("Шевченко Валерий");
-            a.Add("Полухин Максим");
-            a.Add("Круглов Георгий");
-            a.Add("Повышев Владислав");
-            a.Add("Сазиков Михаил");
-            a.Add("Вититнёв Кирилл");
-            GenerateTable("Prog", "M3113", a);
-        }
-        
-        //Позже можно будет давать методу на вводу просто класс Группа
-        public static void GenerateTable(string subject, string groupName, List<string> names)
-        {
-            var workbook = new XLWorkbook();
-            var worksheet = workbook.Worksheets.Add(subject + groupName);
-            
-            worksheet.Cell("A" + 1).Value = "ФИО";
-            worksheet.Cell("A" + 1).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
-            worksheet.Cell("A" + 1).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-            worksheet.Cell("A" + 1).Style.Border.BottomBorder = XLBorderStyleValues.Thick;
-            worksheet.Cell("A" + 1).Style.Border.LeftBorder = XLBorderStyleValues.Thick;
-            worksheet.Cell("A" + 1).Style.Border.RightBorder = XLBorderStyleValues.Thick;
-            worksheet.Cell("A" + 1).Style.Border.TopBorder = XLBorderStyleValues.Thick;
+        private readonly XLWorkbook _workbook = new XLWorkbook();
+        private readonly List<IXLWorksheet> _worksheets = new List<IXLWorksheet>();
 
-            var k = 2;
-            var max = 0;
-            foreach (var i in names)
+        private (char, int) ParsePosition(string position)
+        {
+            char column = ' ';
+            string line = "";
+
+            foreach (var i in position)
             {
-                worksheet.Cell("A" + k).Value = i;
-                if (i.Length > max)
+                if (i >= '0' && i <= '9')
                 {
-                    max = i.Length;
+                    line += i;
                 }
+                else
+                {
+                    column = i;
+                }
+            }
 
-                worksheet.Cell("A" + k).Style.Border.BottomBorder = XLBorderStyleValues.Thin;
-                worksheet.Cell("A" + k).Style.Border.LeftBorder = XLBorderStyleValues.Thin;
-                worksheet.Cell("A" + k).Style.Border.RightBorder = XLBorderStyleValues.Thin;
+            return (column, int.Parse(line));
+        }
+
+        private void AddWorksheet(string nameOfSheet)
+        {
+            _worksheets.Add(_workbook.Worksheets.Add(nameOfSheet));
+        }
+
+        private void SetCellBordersThick(int numberOfSheet, string position)
+        {
+            _worksheets[numberOfSheet].Cell(position).Style.Border.BottomBorder = XLBorderStyleValues.Thick;
+            _worksheets[numberOfSheet].Cell(position).Style.Border.TopBorder = XLBorderStyleValues.Thick;
+            _worksheets[numberOfSheet].Cell(position).Style.Border.RightBorder = XLBorderStyleValues.Thick;
+            _worksheets[numberOfSheet].Cell(position).Style.Border.LeftBorder = XLBorderStyleValues.Thick;
+        }
+
+        private void SetCellsBordersThick(int numberOfSheet, string leftTopPosition, string rightBottomPosition)
+        {
+            char columnLT, columnRB;
+            int lineLT, lineRB;
+            (columnLT, lineLT) = ParsePosition(leftTopPosition);
+            (columnRB, lineRB) = ParsePosition(rightBottomPosition);
+
+            for (char i = columnLT; i < columnRB || i == columnLT; ++i)
+            {
+                for (int j = lineLT; j < lineRB || j == lineLT; ++j)
+                {
+                    SetCellBordersThick(numberOfSheet, i.ToString() + j);
+                }
+            }
+        }
+
+        private void SetCellBordersThin(int numberOfSheet, string position)
+        {
+            _worksheets[numberOfSheet].Cell(position).Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+            _worksheets[numberOfSheet].Cell(position).Style.Border.TopBorder = XLBorderStyleValues.Thin;
+            _worksheets[numberOfSheet].Cell(position).Style.Border.RightBorder = XLBorderStyleValues.Thin;
+            _worksheets[numberOfSheet].Cell(position).Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+        }
+
+        private void SetCellsBordersThin(int numberOfSheet, string leftTopPosition, string rightBottomPosition)
+        {
+            char columnLT, columnRB;
+            int lineLT, lineRB;
+            (columnLT, lineLT) = ParsePosition(leftTopPosition);
+            (columnRB, lineRB) = ParsePosition(rightBottomPosition);
+
+            for (char i = columnLT; i < columnRB || i == columnLT; ++i)
+            {
+                for (int j = lineLT; j < lineRB || j == lineLT; ++j)
+                {
+                    SetCellBordersThin(numberOfSheet, i.ToString() + j);
+                }
+            }
+        }
+
+        private void SetCellAlignmentCenter(int numberOfSheet, string position)
+        {
+            _worksheets[numberOfSheet].Cell(position).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
+            _worksheets[numberOfSheet].Cell(position).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+        }
+
+        private void SetCellsAlignmentCenter(int numberOfSheet, string leftTopPosition, string rightBottomPosition)
+        {
+            char columnLT, columnRB;
+            int lineLT, lineRB;
+            (columnLT, lineLT) = ParsePosition(leftTopPosition);
+            (columnRB, lineRB) = ParsePosition(rightBottomPosition);
+
+            for (char i = columnLT; i < columnRB || i == columnLT; ++i)
+            {
+                for (int j = lineLT; j < lineRB || j == lineLT; ++j)
+                {
+                    SetCellAlignmentCenter(numberOfSheet, i.ToString() + j);
+                }
+            }
+        }
+
+        private void SetCellData(int numberOfSheet, string position, string data)
+        {
+            _worksheets[numberOfSheet].Cell(position).Value = data;
+        }
+
+        private void SetCellsDataVertical(int numberOfSheet, string startCell, List<string> data)
+        {
+            char column;
+            int line;
+            (column, line) = ParsePosition(startCell);
+
+            var k = line;
+            foreach (var i in data)
+            {
+                _worksheets[numberOfSheet].Cell(column.ToString() + k).Value = i;
+                
+                if (i.Length >= _worksheets[numberOfSheet].Column(column.ToString()).Width)
+                {
+                    _worksheets[numberOfSheet].Column(column.ToString()).Width = i.Length + 1;
+                }
                 k++;
             }
-            worksheet.Column("A").Width = max + 1;
-            
-            //Потом можно будет сделать выплёвывание этой таблички на сервер или куда там будет удобней
-            var path = @"E:\ITMO prog\Projects\Sea-ink\Docs\" + subject + groupName + ".xlsx";
+        }
+        
+        private void SetCellsDataHorizontal(int numberOfSheet, string startCell, List<string> data)
+        {
+            char column;
+            int line;
+            (column, line) = ParsePosition(startCell);
+
+            var k = column;
+            foreach (var i in data)
+            {
+                _worksheets[numberOfSheet].Cell(k.ToString() + line).Value = i;
+
+                if (i.Length >= _worksheets[numberOfSheet].Column(k.ToString()).Width)
+                {
+                    _worksheets[numberOfSheet].Column(k.ToString()).Width = i.Length + 1;
+                }
+                k++;
+            }
+        }
+
+        private void SaveWorkbook(string path)
+        {
             if (File.Exists(path))
             {
                 File.Delete(path);
             }
-
-            string s = path;
-            workbook.SaveAs(path);
-            }
-
-        public void SyncGroup(string subject, string groupName)
-        {
-            throw new NotImplementedException();
+            _workbook.SaveAs(path);
         }
 
-        public void SendPoints(string subject, string groupName)
+        public void GenerateWorkbook(string subject, List<string> names, List<string> labs)
         {
-            throw new NotImplementedException();
+            //TODO: Ширина таблицы ограничена английским алфавитом - когда-то исправить
+            AddWorksheet(subject);
+
+            SetCellData(0, "A" + 1, "ФИО");
+            SetCellBordersThick(0, "A" + 1);
+            SetCellAlignmentCenter(0, "A" + 1);
+
+            SetCellsDataVertical(0, "A" + 2, names);
+            SetCellsDataHorizontal(0, "B" + 1, labs);
+
+            var lastCell = Convert.ToChar('B' + labs.Count).ToString() + (2 + names.Count);
+            SetCellsBordersThin(0, "A" + 2, lastCell);
+            
+            lastCell = Convert.ToChar('B' + labs.Count).ToString() + 1;
+            SetCellsBordersThick(0, "B" + 1, lastCell);
+            SetCellsAlignmentCenter(0, "B" + 1, lastCell);
+
+            var path = @"E:\ITMO prog\Projects\Sea-ink\Docs\" + subject + ".xlsx";
+            SaveWorkbook(path);
         }
     }
 }
