606662503b76c24a358b8243cb82ecca0dbe2b94
Георгий <71693183+ronimizy@users.noreply.github.com>
2021-04-11 22:48:46 +0300

    hothotfix

--- Source/SeaInk.Core/Entities/Division.cs
+++ Source/SeaInk.Core/Entities/Division.cs
@@ -5,14 +5,16 @@ namespace SeaInk.Core.Entities
 {
     public class Division
     {
-        public Subject Subject { get; set; } = new Subject();
-        public List<StudyGroup> Groups { get; set; } = new();
+        public Subject Subject { get; set; }
+        public List<StudyGroup> Groups { get; set; }
 
-        public string TableId { get; set; } = "";
+        public string TableId { get; set; }
 
         public Division()
         {
-            
+            Subject = new Subject();
+            Groups = new List<StudyGroup>();
+            TableId = null;
         }
         public Division(Subject subject, List<StudyGroup> groups, string tableId)
         {
--- Source/SeaInk.Core/Entities/StudentAssignmentProgress.cs
+++ Source/SeaInk.Core/Entities/StudentAssignmentProgress.cs
@@ -4,9 +4,9 @@ namespace SeaInk.Core.Entities
 {
     public class StudentAssignmentProgress
     {
-        public Student? Student { get; set; }
-        public StudyAssignment? Assignment { get; set; }
-        public AssignmentProgress? Progress { get; set; }
+        public Student Student { get; set; }
+        public StudyAssignment Assignment { get; set; }
+        public AssignmentProgress Progress { get; set; }
 
         public StudentAssignmentProgress()
         {
