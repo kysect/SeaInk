9c8ba24d78bce3f6263d06c93f6c02a638b415a4
Георгий <71693183+ronimizy@users.noreply.github.com>
2021-04-11 23:27:21 +0300

    resolved sth

--- Source/SeaInk.Sample/Program.cs
+++ Source/SeaInk.Sample/Program.cs
@@ -8,22 +8,6 @@ namespace SeaInk.Sample
     {
         public static void Main(string[] args)
         {
-            var table = new GoogleTable();
-            table.Load("1Z7JaEecXh5K6NPwrRuZC0OfM3TMCzHt-_POD2812J_k");
-            
-            table.SetValuesForCellsAt(new TableIndex("Test", 1, 1, 1), new List<IList<object>>
-            {
-                new List<object>
-                {
-                    "я", "пофиксил", "задание", "данных"
-                },
-                new List<object>
-                {
-                    "для", "ячеек", "но, надо много ", "переделывать("
-                }
-            });
-            
-            // Console.WriteLine(table.Create("Name"));
         }
     }
 }
