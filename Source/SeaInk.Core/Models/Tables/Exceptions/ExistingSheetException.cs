e5801100e6088c723a07ca9b0c01f27d4d645379
Георгий <71693183+ronimizy@users.noreply.github.com>
2021-04-14 11:44:59 +0300

    refactor: TableException's parameterless constructors overload refactor: DefaultCellStyle.HyperLink cringe resolved

--- Source/SeaInk.Core/Models/Tables/Exceptions/ExistingSheetException.cs
+++ Source/SeaInk.Core/Models/Tables/Exceptions/ExistingSheetException.cs
@@ -2,8 +2,13 @@ namespace SeaInk.Core.Models.Tables.Exceptions
 {
     public class ExistingSheetException : TableException
     {
-        public ExistingSheetException(string message = "") 
+        public ExistingSheetException(string message) 
             : base($"Sheet with specified signature already exists {message}")
+        {
+        }
+
+        public ExistingSheetException()
+            : this("")
         {
         }
     }
--- Source/SeaInk.Core/Models/Tables/Exceptions/NonExistingIndexException.cs
+++ Source/SeaInk.Core/Models/Tables/Exceptions/NonExistingIndexException.cs
@@ -4,8 +4,13 @@ namespace SeaInk.Core.Models.Tables.Exceptions
 {
     public class NonExistingIndexException : TableException
     {
-        public NonExistingIndexException(string message = "") 
+        public NonExistingIndexException(string message)
             : base($"Specified index does not exists {message}")
+        {
+        }
+
+        public NonExistingIndexException()
+            : this("")
         {
         }
     }
