753942faa4a2e43bc82b3477c67ce618c77b130f
Георгий <71693183+ronimizy@users.noreply.github.com>
2021-04-15 12:31:58 +0300

    feat: Implemented ITable refactor: TableIndexRange.cs and ICellStyle.cs

--- Source/SeaInk.Sample/Program.cs
+++ Source/SeaInk.Sample/Program.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Drawing;
+using SeaInk.Core.Entities.Tables;
 using SeaInk.Core.Models.Tables;
+using SeaInk.Core.Models.Tables.Styles;
 
 namespace SeaInk.Sample
 {
@@ -7,11 +11,68 @@ namespace SeaInk.Sample
     {
         public static void Main(string[] args)
         {
-            var range = new TableIndexRange("A", 0, (0, 0), (5, 5));
+            ITable table = new GoogleTable();
+            table.Load("1Z7JaEecXh5K6NPwrRuZC0OfM3TMCzHt-_POD2812J_k");
 
-            foreach (TableIndex index in range)
+            var sheet = new TableIndex("NEW", table.SheetCount + 1);
+
+            try
             {
-                Console.WriteLine(index);
+                table.CreateSheet(sheet);
+                table.SetValuesForCellsAt(sheet, 
+                    new List<List<string>>
+                    {
+                        new ()
+                        {
+                            "Проверка", "Вставки"
+                        },
+                        new ()
+                        {
+                            "Строковых", "Значений"
+                        }
+                    });
+            
+                table.SetValuesForCellsAt(sheet.WithColumn(3), 
+                    new List<List<int>>
+                    {
+                        new ()
+                        {
+                            10, 20
+                        },
+                        new ()
+                        {
+                            30, 40
+                        }
+                    });
+            
+                table.Rename("rename test");
+                table.RenameSheet(sheet, "NEW NEW");
+                table.FormatCellsAt(new TableIndexRange(sheet, sheet.WithColumn(1).WithRow(1)), new DefaultCellStyle
+                {
+                    BackgroundColor = Color.Aqua
+                });
+                table.FormatCellAt(sheet.WithColumn(3), new DefaultCellStyle
+                {
+                    BackgroundColor = Color.Red
+                });
+                table.MergeCellsAt(new TableIndexRange(sheet.SheetName, sheet.SheetId, (0, 3), (0, 4)));
+
+                Console.WriteLine("Press ENTER to delete row");
+                Console.ReadLine();
+                table.DeleteRowAt(sheet.WithRow(1));
+                
+                Console.WriteLine("Press ENTER to delete column");
+                Console.ReadLine();
+                table.DeleteColumnAt(sheet.WithColumn(3));
+                
+                Console.WriteLine("Press ENTER to delete sheet");
+                Console.ReadLine();
+                table.DeleteSheet(sheet);
+            }
+            catch (Exception)
+            {
+                table.DeleteSheet(sheet);
+                throw;
             }
         }
     }
