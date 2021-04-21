360c7f13e211e06eadb75ed2b35c1e8c0d2445e0
Георгий <71693183+ronimizy@users.noreply.github.com>
2021-04-21 15:24:40 +0300

    refactor: Logger.cs colored console log

--- Source/SeaInk.Core/Utils/Logger.cs
+++ Source/SeaInk.Core/Utils/Logger.cs
@@ -4,11 +4,18 @@ namespace SeaInk.Core.Utils
 {
     public static class Logger
     {
-        public static Action<string> LogMethod { get; set; } = Console.WriteLine;
+        public static Action<string> LogMethod { get; set; } = ConsoleLog;
 
         public static void Log(object message)
         {
             LogMethod(message.ToString());
+        }
+
+        private static void ConsoleLog(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
 }
