c699a0867d20465ef47554e3c4a5c813fa899834
Fredi Kats <fredikats@outlook.com>
2021-04-28 17:33:43 +0300

    refactor: move table logic to separated folder

--- Source/SeaInk.Core/Models/Google/GoogleFileTypes.cs
+++
@@ -1,8 +0,0 @@
-namespace SeaInk.Core.Models.Google
-{
-    public static class GoogleFileTypes
-    {
-        public static readonly string Folder = @"application/vnd.google-apps.folder";
-        public static readonly string GoogleSpreadsheet = @"application/vnd.google-apps.spreadsheet";
-    }
-}
---
+++ Source/SeaInk.Core/TableIntegrations/Models/Exceptions/ExistingSheetException.cs
@@ -0,0 +1,15 @@
+namespace SeaInk.Core.TableIntegrations.Models.Exceptions
+{
+    public class ExistingSheetException : TableException
+    {
+        public ExistingSheetException(string message) 
+            : base($"Sheet with specified signature already exists {message}")
+        {
+        }
+
+        public ExistingSheetException()
+            : this("")
+        {
+        }
+    }
+}
