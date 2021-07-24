37efbf4dc7919bb4f6337fad24444ef51db88e98
Bill <eid20021@gmail.com>
2021-07-24 07:43:14 +0300

    refactor: Mentor Controller rename

--- Source/SeaInk.Endpoints/Server/Controllers/MentorController.cs
+++ Source/SeaInk.Endpoints/Server/Controllers/MentorController.cs
@@ -9,23 +9,23 @@ using SeaInk.Core.Entities;
 namespace SeaInk.Endpoints.Server.Controllers
 {
     [Route("[Controller]")]
-    public class ApiController:Controller
+    public class MentorController:Controller
     {
         private readonly IUniversitySystemApi _api;
 
-        public ApiController()
+        public MentorController()
         {
             _api = new FakeUniversitySystemApi();
         }
 
-        [HttpGet("mentors/{mentorId}/subjects")]
+        [HttpGet("{mentorId}/subjects")]
         public List<Subject> GetSubjectsList(int mentorId)
         {
             List<Division> mentorDivisions = _api.GetMentorBySystemId(mentorId).Divisions;
             return mentorDivisions.Select(x => x.Subject).ToList();
         }
         
-        [HttpGet("mentors/{mentorId}/subjects/{subjectId}/groups")]
+        [HttpGet("{mentorId}/subject/{subjectId}/groups")]
         public List<StudyGroup> GetGroupsList(int mentorId, int subjectId)
         {
             List<Division> mentorDivisions = _api.GetMentorBySystemId(mentorId).Divisions;
