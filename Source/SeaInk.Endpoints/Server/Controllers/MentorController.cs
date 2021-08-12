c53f1440e9df50506054f1b30b0f5a75dde0c511
Bill <eid20021@gmail.com>
2021-08-12 18:31:41 +0300

    refactor: MentorController try/catch remove

--- Source/SeaInk.Endpoints/Server/Controllers/MentorController.cs
+++ Source/SeaInk.Endpoints/Server/Controllers/MentorController.cs
@@ -20,39 +20,39 @@ namespace SeaInk.Endpoints.Server.Controllers
         {
             _api = universitySystemApi;
         }
-        
+
 
         [HttpGet("{mentorId}/subjects")]
         public List<SubjectDto> GetSubjectsList(int mentorId)
         {
-            try
-            {
-                List<Division> mentorDivisions = _api.GetMentor(mentorId).Divisions;
-                return mentorDivisions.Select(x => new SubjectDto(x.Subject)).ToList();
-            }
-            catch (NullReferenceException)
-            {
-                return new List<SubjectDto>();
-            }
+    
+        Mentor mentor = _api.GetMentor(mentorId);
+        if (mentor is null)
+        {
+            return new List<SubjectDto>();
+        }
+
+        List<Division> mentorDivisions = mentor.Divisions;
+        return mentorDivisions.Select(x => new SubjectDto(x.Subject)).ToList();
         }
         
         [HttpGet("{mentorId}/subject/{subjectId}/groups")]
         public List<StudyGroupDto> GetGroupsList(int mentorId, int subjectId)
         {
-            try
-            {
-                List<Division> mentorDivisions = _api.GetMentor(mentorId).Divisions;
-                return mentorDivisions
-                    .Where(division => division.Subject.Id == subjectId)
-                    .SelectMany(division => division.Groups)
-                    .DistinctBy(group => group.Id)
-                    .Select(x => new StudyGroupDto(x))
-                    .ToList();
-            }
-            catch (NullReferenceException)
+            Mentor mentor = _api.GetMentor(mentorId);
+            
+            if (mentor is null)
             {
                 return new List<StudyGroupDto>();
             }
+            
+            List<Division> mentorDivisions = _api.GetMentor(mentorId).Divisions;
+            return mentorDivisions
+                .Where(division => division.Subject.Id == subjectId)
+                .SelectMany(division => division.Groups)
+                .DistinctBy(group => group.Id)
+                .Select(x => new StudyGroupDto(x))
+                .ToList();
         }
     }
 }
