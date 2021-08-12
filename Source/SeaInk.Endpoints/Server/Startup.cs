f0370f5685ffa1f1d3abb99479c38e1416e2928d
Георгий <71693183+ronimizy@users.noreply.github.com>
2021-08-12 14:45:44 +0300

    fix: moved service inject to a proper place

--- Source/SeaInk.Endpoints/Server/Startup.cs
+++ Source/SeaInk.Endpoints/Server/Startup.cs
@@ -1,8 +1,10 @@
+using Infrastructure.APIs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SeaInk.Core.APIs;
 
 namespace SeaInk.Endpoints.Server
 {
@@ -19,7 +21,7 @@ namespace SeaInk.Endpoints.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IUniversitySystemApi, FakeUniversitySystemApi>();
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddSwaggerGen();
