51ea7b16bb6158951615d997bc74f1b8389e9d0a
Fredi Kats <fredikats@outlook.com>
2021-08-29 11:01:54 +0300

    fix: conflicts after merge

--- Source/SeaInk.Endpoints/SeaInk.Endpoints.Sdk/ApiClients/SeaInkApiClient.cs
+++ Source/SeaInk.Endpoints/SeaInk.Endpoints.Sdk/ApiClients/SeaInkApiClient.cs
@@ -272,6 +272,84 @@ namespace SeaInk.Endpoints.Sdk
             }
         }
     
+        /// <returns>Success</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        public System.Threading.Tasks.Task<System.Collections.Generic.ICollection<DivisionDto>> DivisionsAsync(int mentorId)
+        {
+            return DivisionsAsync(mentorId, System.Threading.CancellationToken.None);
+        }
+    
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>Success</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        public async System.Threading.Tasks.Task<System.Collections.Generic.ICollection<DivisionDto>> DivisionsAsync(int mentorId, System.Threading.CancellationToken cancellationToken)
+        {
+            if (mentorId == null)
+                throw new System.ArgumentNullException("mentorId");
+    
+            var urlBuilder_ = new System.Text.StringBuilder();
+            urlBuilder_.Append("mentors/{mentorId}/divisions");
+            urlBuilder_.Replace("{mentorId}", System.Uri.EscapeDataString(ConvertToString(mentorId, System.Globalization.CultureInfo.InvariantCulture)));
+    
+            var client_ = _httpClient;
+            var disposeClient_ = false;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    request_.Method = new System.Net.Http.HttpMethod("GET");
+                    request_.Headers.Accept.Add(System.Net.Http.Headers.MediaTypeWithQualityHeaderValue.Parse("text/plain"));
+    
+                    PrepareRequest(client_, request_, urlBuilder_);
+    
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+    
+                    PrepareRequest(client_, request_, url_);
+    
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    var disposeResponse_ = true;
+                    try
+                    {
+                        var headers_ = System.Linq.Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+    
+                        ProcessResponse(client_, response_);
+    
+                        var status_ = (int)response_.StatusCode;
+                        if (status_ == 200)
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<System.Collections.Generic.ICollection<DivisionDto>>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                            if (objectResponse_.Object == null)
+                            {
+                                throw new ApiException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                            }
+                            return objectResponse_.Object;
+                        }
+                        else
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new ApiException("The HTTP status code of the response was not expected (" + status_ + ").", status_, responseData_, headers_, null);
+                        }
+                    }
+                    finally
+                    {
+                        if (disposeResponse_)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+                if (disposeClient_)
+                    client_.Dispose();
+            }
+        }
+    
         protected struct ObjectResponseResult<T>
         {
             public ObjectResponseResult(T responseObject, string responseText)
@@ -384,17 +462,8 @@ namespace SeaInk.Endpoints.Sdk
         [System.Text.Json.Serialization.JsonPropertyName("spreadsheetId")]
         public string SpreadsheetId { get; set; }
     
-        [System.Text.Json.Serialization.JsonPropertyName("mentorId")]
-        public int MentorId { get; set; }
-    
-        [System.Text.Json.Serialization.JsonPropertyName("mentorName")]
-        public string MentorName { get; set; }
-    
-        [System.Text.Json.Serialization.JsonPropertyName("subject")]
-        public SubjectDto Subject { get; set; }
-    
-        [System.Text.Json.Serialization.JsonPropertyName("groups")]
-        public System.Collections.Generic.ICollection<StudyGroupDto> Groups { get; set; }
+        [System.Text.Json.Serialization.JsonPropertyName("studyGroupSubjects")]
+        public System.Collections.Generic.ICollection<StudyGroupSubjectDto> StudyGroupSubjects { get; set; }
     
     
     }
@@ -420,11 +489,8 @@ namespace SeaInk.Endpoints.Sdk
         [System.Text.Json.Serialization.JsonPropertyName("fullName")]
         public string FullName { get; set; }
     
-        [System.Text.Json.Serialization.JsonPropertyName("subjects")]
-        public System.Collections.Generic.ICollection<SubjectDto> Subjects { get; set; }
-    
-        [System.Text.Json.Serialization.JsonPropertyName("divisions")]
-        public System.Collections.Generic.ICollection<DivisionDto> Divisions { get; set; }
+        [System.Text.Json.Serialization.JsonPropertyName("studyGroupSubjects")]
+        public System.Collections.Generic.ICollection<StudyGroupSubjectDto> StudyGroupSubjects { get; set; }
     
     
     }
@@ -497,6 +563,24 @@ namespace SeaInk.Endpoints.Sdk
     
         [System.Text.Json.Serialization.JsonPropertyName("students")]
         public System.Collections.Generic.ICollection<StudentDto> Students { get; set; }
+    
+    
+    }
+    
+    [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "10.4.4.0 (Newtonsoft.Json v11.0.0.0)")]
+    public partial class StudyGroupSubjectDto 
+    {
+        [System.Text.Json.Serialization.JsonPropertyName("id")]
+        public int Id { get; set; }
+    
+        [System.Text.Json.Serialization.JsonPropertyName("studyGroup")]
+        public StudyGroupDto StudyGroup { get; set; }
+    
+        [System.Text.Json.Serialization.JsonPropertyName("subject")]
+        public SubjectDto Subject { get; set; }
+    
+        [System.Text.Json.Serialization.JsonPropertyName("mentorIds")]
+        public System.Collections.Generic.ICollection<int> MentorIds { get; set; }
     
     
     }
