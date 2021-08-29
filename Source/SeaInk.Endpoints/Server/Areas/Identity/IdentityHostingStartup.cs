2926ea3a85dad242721b8a71e326d12e2e64ad6f
Fredi Kats <fredikats@outlook.com>
2021-08-29 18:48:04 +0300

    feat: generate identity pages

---
+++ Source/SeaInk.Endpoints/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SeaInk.Endpoints.Server.Authorization;
+
+[assembly: HostingStartup(typeof(SeaInk.Endpoints.Server.Areas.Identity.IdentityHostingStartup))]
+namespace SeaInk.Endpoints.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
