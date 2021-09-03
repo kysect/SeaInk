7af2008943401e06f0dd57614d704152ccd7dbfe
Valery Shevchenko <ar4er.valera@gmail.com>
2021-09-03 18:39:38 +0300

    fix: async methods

--- Source/SeaInk.Core/TableGeneration/Authorisation.cs
+++ Source/SeaInk.Core/TableGeneration/Authorisation.cs
@@ -4,36 +4,66 @@ using System.Threading.Tasks;
 using Kysect.CentumFramework.Drive;
 using Kysect.CentumFramework.Drive.Actions.Models;
 using Kysect.CentumFramework.Drive.Entities;
+using Kysect.CentumFramework.Drive.Extensions;
 using Kysect.CentumFramework.Drive.Models;
 using Kysect.CentumFramework.Drive.Models.Query;
 using Kysect.CentumFramework.Sheets;
 using Kysect.CentumFramework.Sheets.Entities;
 using Kysect.CentumFramework.Utility.Authorization;
+using File = Kysect.CentumFramework.Drive.Entities.File;
 
 namespace SeaInk.Core.TableGeneration
 {
-    public class Authorisation
+    public class Authorisation : AuthorisationBase
     {
-        public static async Task<Sheet> AuthorizeSheet()
+        protected override async Task<AuthorisationService> GetAuthorisationService()
         {
-            var authorisation = await AuthorisationService.CreateAsync("Test Application",
+            return await AuthorisationService.CreateAsync("Test Application",
                 "user",
                 new FileStream("Secrets.json", FileMode.Open, FileAccess.Read),
                 "token.json",
                 new[] {Scope.Drive, Scope.Spreadsheets});
+        }
 
-            var driveService = new DriveService(authorisation);
-            var sheetsService = new SheetsService(authorisation);
+        protected override DriveService GetDriveService(AuthorisationService authorisation)
+        {
+            return new DriveService(authorisation);
+        }
 
-            //Всё в тестовом варианте, потом это будет на сервере, нужно будет исправить разумеется
-            QueryCondition condition = QueryTerm.Name.Equal("Test Folder");
-            var configuration = new ListActionConfiguration(condition);
-            var folder = (Folder) driveService.FindFiles(configuration).Result.Single();
+        protected override SheetsService GetSheetService(AuthorisationService authorisation)
+        {
+            return new SheetsService(authorisation);
+        }
 
-            var sheetDescriptor = new FileDescriptor("Test sheet", FileType.Spreadsheet, folder);
-            var sheetFile = driveService.CreateFile(sheetDescriptor);
-            var spreadsheet = sheetsService.GetSpreadsheet(sheetFile);
-            return spreadsheet[0];
+        protected override QueryCondition GetQueryCondition()
+        {
+            return QueryTerm.Name.Equal("Test Folder");
+        }
+
+        protected override ListActionConfiguration GetListActionConfiguration(QueryCondition queryCondition)
+        {
+            return new ListActionConfiguration(queryCondition);
+        }
+
+        protected override async Task<Folder> GetFolder(DriveService driveService, ListActionConfiguration listActionConfiguration)
+        {
+            var findFiles = await driveService.FindFilesAsync(listActionConfiguration);
+            return (Folder) findFiles.Result.Single();
+        }
+
+        protected override FileDescriptor GetFileDescriptor(Folder folder)
+        {
+            return new FileDescriptor("Test sheet", FileType.Spreadsheet, folder);
+        }
+
+        protected override async Task<File> GetFile(DriveService driveService, FileDescriptor fileDescriptor)
+        {
+            return await driveService.CreateFileAsync(fileDescriptor);
+        }
+
+        protected override async Task<Spreadsheet> GetSpreadsheet(SheetsService sheetsService, File file)
+        {
+            return await sheetsService.GetSpreadsheetAsync(file);
         }
     }
 }
