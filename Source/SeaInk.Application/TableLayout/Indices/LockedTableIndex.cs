6dffcbb8c0ef716ac598d548de22214dbbe5ab55
Георгий <71693183+ronimizy@users.noreply.github.com>
2021-11-27 17:33:27 +0300

    feat: indices

---
+++ Source/SeaInk.Application/TableLayout/Indices/LockedTableIndex.cs
@@ -0,0 +1,53 @@
+using System;
+using SeaInk.Application.TableLayout.Models;
+using SeaInk.Utility.Extensions;
+
+namespace SeaInk.Application.TableLayout.Indices
+{
+    public class LockedTableIndex : ITableIndex
+    {
+        private readonly ITableIndex _index;
+        private readonly ITableIndex _begin;
+        private readonly Frame _frame;
+
+        public LockedTableIndex(ITableIndex index, Frame frame)
+        {
+            _index = index.ThrowIfNull(nameof(index));
+            _begin = index.Copy();
+            _frame = frame.ThrowIfNull(nameof(frame));
+        }
+
+        private LockedTableIndex(ITableIndex index, ITableIndex begin, Frame frame)
+        {
+            _index = index;
+            _begin = begin;
+            _frame = frame;
+        }
+
+        public int Column => _index.Column;
+        public int Row => _index.Row;
+
+        public void MoveHorizontally(int i = 1)
+        {
+            _index.MoveHorizontally(i);
+
+            int diff = _index.Column - _begin.Column;
+
+            if (diff < 0 || diff >= _frame.Width)
+                throw new IndexOutOfRangeException();
+        }
+
+        public void MoveVertically(int i = 1)
+        {
+            _index.MoveVertically(i);
+
+            int diff = _index.Row - _begin.Row;
+
+            if (diff < 0 || diff >= _frame.Height)
+                throw new IndexOutOfRangeException();
+        }
+
+        public ITableIndex Copy()
+            => new LockedTableIndex(_index.Copy(), _begin, _frame);
+    }
+}
---
+++ Source/SeaInk.Application/TableLayout/Indices/ScaledTableIndex.cs
@@ -0,0 +1,28 @@
+using SeaInk.Application.TableLayout.Models;
+
+namespace SeaInk.Application.TableLayout.Indices
+{
+    public class ScaledTableIndex : ITableIndex
+    {
+        private readonly Scale _scale;
+        private readonly ITableIndex _index;
+
+        public ScaledTableIndex(Scale scale, ITableIndex index)
+        {
+            _scale = scale;
+            _index = index;
+        }
+
+        public int Column => _index.Column;
+        public int Row => _index.Row;
+
+        public void MoveHorizontally(int i = 1)
+            => _index.MoveHorizontally(i * _scale.Horizontal);
+
+        public void MoveVertically(int i = 1)
+            => _index.MoveVertically(i * _scale.Vertical);
+
+        public ITableIndex Copy()
+            => new ScaledTableIndex(_scale, _index.Copy());
+    }
+}
