d2f96e2734d8d0f72a87c413228e9234ea2a42d5
Георгий <71693183+ronimizy@users.noreply.github.com>
2021-11-27 17:42:59 +0300

    refactor: resolver exception code smells

--- Source/SeaInk.Application/Exceptions/InvalidScaleComponentException.cs
+++ Source/SeaInk.Application/Exceptions/InvalidScaleComponentException.cs
@@ -1,10 +1,16 @@
+using System;
+using System.Runtime.Serialization;
 using SeaInk.Core.Exceptions;
 
 namespace SeaInk.Application.Exceptions
 {
+    [Serializable]
     public class InvalidScaleComponentException : SeaInkException
     {
         public InvalidScaleComponentException(string argName, int value)
             : base($"{argName} must be positive. Value {value}") { }
+
+        protected InvalidScaleComponentException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
     }
 }
