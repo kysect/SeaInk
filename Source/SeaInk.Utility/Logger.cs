1b83bf8a088d679e182521f0e3d5c618e6bced85
ronimizy <71693183+ronimizy@users.noreply.github.com>
2021-12-09 11:23:56 +0300

    refactor: logger warning

--- Source/SeaInk.Utility/Logger.cs
+++ Source/SeaInk.Utility/Logger.cs
@@ -7,7 +7,7 @@ namespace SeaInk.Utility
         public static Action<string> LogMethod { get; set; } = ConsoleLog;
 
         public static void Log(object message)
-            => LogMethod(message.ToString());
+            => LogMethod(message.ToString() ?? string.Empty);
 
         private static void ConsoleLog(string message)
         {
